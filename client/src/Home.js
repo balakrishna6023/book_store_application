14cf230076959eb3109e981ba64a651f61e2cea8
balakrishna6023 <128021148+balakrishna6023@users.noreply.github.com>
2024-11-20 22:41:31 +0530

    Update Home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./App.css";
 
-const API_URL = "http://localhost:5000/books";
+const API_URL = "https://book-store-application-server.onrender.com/books";
 
 function Home() {
   const [books, setBooks] = useState([]);
